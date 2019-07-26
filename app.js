9a9f71587f1e8aaddb08014caf75dd5aeced529d
chenjiaobin <chenjb@keketour.com>
2019-07-26 18:09:27 +0800

    添加云开发配置，暂未使用

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 //app.js
 App({
   onLaunch: function () {
-    
+    // wx.cloud.init({
+    //   env: 'gh-weapp-test-c46p5',
+    //   traceUser: true
+    // })
   },
   globalData: {
     userInfo: null,
